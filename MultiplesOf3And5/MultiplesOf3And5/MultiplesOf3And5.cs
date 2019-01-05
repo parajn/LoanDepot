dbbc1491870f9ee4ebde997ff61f2634caa84931
parajn <35551398+parajn@users.noreply.github.com>
2019-01-04 19:53:03 -0600

    Create MultiplesOf3And5.cs
    missing file

---
+++ MultiplesOf3And5/MultiplesOf3And5/MultiplesOf3And5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    class MultiplesOf3And5
+    {
+        static void Main(string[] args)
+        {
+            MultiplesOf3And5 multipleObj = new MultiplesOf3And5();
+            Console.WriteLine("Sum of all the multiples of 3 or 5 below 1000: "
+                + multipleObj.FindSumOfAllTheMultiplesOf3And5Below1000());
+
+        }
+        private long FindSumOfAllTheMultiplesOf3And5Below1000()
+        {
+            long sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+    }
+}
